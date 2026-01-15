717e66cea6ae10a5235b793011ab8a4e428b9759
lucasmoraist <luksmnt1101@gmail.com>
2026-01-15 16:42:07 -0300

    first commit

---
+++ src/main/java/com/lucasmoraist/transfer_manager/TransferManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.transfer_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoraist/transfer_manager/TransferManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.transfer_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
