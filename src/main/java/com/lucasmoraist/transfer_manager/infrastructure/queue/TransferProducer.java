b090010208a1a99ab89c9057aba8558f11c82a9b
lucasmoraist <luksmnt1101@gmail.com>
2026-01-15 22:39:18 -0300

    feat: add TransferProducer for sending messages to the message queue

---
+++ src/main/java/com/lucasmoraist/transfer_manager/infrastructure/queue/TransferProducer.java
@@ -0,0 +1,21 @@
+package com.lucasmoraist.transfer_manager.infrastructure.queue;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class TransferProducer {
+
+    private final StreamBridge streamBridge;
+
+    public void sendMessage(Message<?> message, String bindingName) {
+        log.info("Sending message [{}] to binding [{}]", message, bindingName);
+        streamBridge.send(bindingName, message);
+    }
+
+}
