7476ccae55d733b5c39eb129b42f14d0079d87e6
lucasmoraist <luksmnt1101@gmail.com>
2026-01-15 22:58:05 -0300

    feat: add updatePayment method to TransferPersistence for processing payment updates

--- src/main/java/com/lucasmoraist/transfer_manager/application/gateway/TransferPersistence.java
+++ src/main/java/com/lucasmoraist/transfer_manager/application/gateway/TransferPersistence.java
@@ -1,7 +1,9 @@
 package com.lucasmoraist.transfer_manager.application.gateway;
 
+import com.lucasmoraist.transfer_manager.domain.message.PaymentMessage;
 import com.lucasmoraist.transfer_manager.domain.model.Transfer;
 
 public interface TransferPersistence {
     Transfer save(Transfer transfer);
+    void updatePayment(PaymentMessage paymentMessage);
 }
--- src/main/java/com/lucasmoraist/transfer_manager/infrastructure/database/persistence/TransferPersistenceImpl.java
+++ src/main/java/com/lucasmoraist/transfer_manager/infrastructure/database/persistence/TransferPersistenceImpl.java
@@ -1,6 +1,7 @@
 package com.lucasmoraist.transfer_manager.infrastructure.database.persistence;
 
 import com.lucasmoraist.transfer_manager.application.gateway.TransferPersistence;
+import com.lucasmoraist.transfer_manager.domain.message.PaymentMessage;
 import com.lucasmoraist.transfer_manager.domain.model.Transfer;
 import com.lucasmoraist.transfer_manager.infrastructure.database.entity.TransferEntity;
 import com.lucasmoraist.transfer_manager.infrastructure.database.repository.TransferRepository;
@@ -8,6 +9,8 @@ import com.lucasmoraist.transfer_manager.infrastructure.mapper.TransferMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Log4j2
 @Component
@@ -21,6 +24,17 @@ public class TransferPersistenceImpl implements TransferPersistence {
         TransferEntity entity = TransferMapper.toEntity(transfer);
         TransferEntity savedEntity = this.transferRepository.save(entity);
         return TransferMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public void updatePayment(PaymentMessage paymentMessage) {
+        TransferEntity entity = this.transferRepository.findById(paymentMessage.transferId())
+                .orElseThrow(() -> new IllegalArgumentException("Transfer not found with id: " + paymentMessage.transferId()));
+
+        entity.setStatus(paymentMessage.status());
+        entity.setProcessedAt(LocalDateTime.now());
+
+        this.transferRepository.save(entity);
     }
 
 }
